c7010317538110109d5fda0126c80a5bdd66d421
Mohammad Faisal <mohammad.faisal@strativ.se>
2023-05-04 19:12:00 +0600

    Compute a validity date for offers

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -1,14 +1,36 @@
-from odoo import models, fields
+import datetime
+from odoo import models, fields, api
 
 class PropertyOffer(models.Model):
-    _name = "estate.property.offer"
-    _description = "A simple description about property offer"
+   _name = "estate.property.offer"
+   _description = "A simple description about property offer"
 
-    price = fields.Float()
-    status = fields.Selection(
+   price = fields.Float()
+   status = fields.Selection(
        copy=False,
        default="accepted",
        selection=[("accepted", "Accepted"), ("refused", "Refused")]
-    )
-    partner_id = fields.Many2one("res.partner", required=True)
-    property_id = fields.Many2one("estate.property", required=True)
+   )
+   partner_id = fields.Many2one("res.partner", required=True)
+   property_id = fields.Many2one("estate.property", required=True)
+
+   date_deadline = fields.Date(
+      compute="_compute_date_deadline",
+      inverse='_inverse_date_deadline',
+      tracking=40, index=True,
+      readonly=False, store=True
+   )
+
+   validity = fields.Integer()
+
+   @api.depends("create_date", "validity")
+   def _compute_date_deadline(self):
+      for record in self:
+         record.date_deadline = record.create_date + datetime.timedelta(days=record.validity)
+
+   def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
+
+
+

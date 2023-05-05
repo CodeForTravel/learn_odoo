1487383f70e00950c85592ce191cf0666e5be19d
Mohammad Faisal <mohammad.faisal@strativ.se>
2023-05-05 23:11:09 +0600

    Add manual ordering

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -4,10 +4,11 @@ from odoo import models, fields
 class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "A simple property type model."
-    _order = "name"
+    _order = 'sequence, name'
 
     name = fields.Char(string="Property Type", default="House", required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
 
     #SQL constraints.
     _sql_constraints = [
